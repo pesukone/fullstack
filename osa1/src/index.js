9cdad4c81e232bdb5c2fdd71136189168f912564
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-01-16 16:56:44 +0200

    1.1

---
+++ osa1/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto
+        o1={osa1} t1={tehtavia1}
+        o2={osa2} t2={tehtavia2}
+        o3={osa3} t3={tehtavia3}
+      />
+      <Yhteensa
+        t1={tehtavia1}
+        t2={tehtavia2}
+        t3={tehtavia3}
+      />
+    </div>
+  )
+}
+
+const Otsikko = ({ kurssi }) => <h1>{kurssi}</h1>
+
+const Sisalto = ({ o1, t1, o2, t2, o3, t3 }) => {
+  return (
+    <div>
+      <p>{o1} {t1}</p>
+      <p>{o2} {t2}</p>
+      <p>{o3} {t3}</p>
+    </div>
+  )
+}
+
+const Yhteensa = ({t1, t2, t3 }) =>
+  <p>yhteensä {t1 + t2 + t3} tehtävää</p>
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
