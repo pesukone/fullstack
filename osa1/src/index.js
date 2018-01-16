fed4ac013498d41d1b9dd7de30432ad743214fef
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-01-16 17:42:40 +0200

    1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,44 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
-    {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    },
-    {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    },
-    {
-      nimi: 'Komponenttien tila',
-      tehtavia: 7
-    }
-  ]
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 7
+      }
+    ]
+  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osat={osat} />
-      <Yhteensa osat={osat} />
+      <Sisalto kurssi={kurssi} />
+      <Yhteensa kurssi={kurssi} />
     </div>
   )
 }
 
-const Otsikko = ({ kurssi }) => <h1>{kurssi}</h1>
+const Otsikko = ({ kurssi }) => <h1>{kurssi.nimi}</h1>
 
-const Sisalto = ({ osat }) => {
+const Sisalto = ({ kurssi }) => {
+  console.log(kurssi)
   return (
     <div>
-      <Osa osa={osat[0]} />
-      <Osa osa={osat[1]} />
-      <Osa osa={osat[2]} />
+      <Osa osa={kurssi.osat[0]} />
+      <Osa osa={kurssi.osat[1]} />
+      <Osa osa={kurssi.osat[2]} />
     </div>
   )
 }
 
-const Yhteensa = ({ osat }) => <p>yhteensä {osat[0].tehtavia+ osat[1].tehtavia + osat[2].tehtavia} tehtävää</p>
+const Yhteensa = ({ kurssi }) => <p>yhteensä {kurssi.osat[0].tehtavia + kurssi.osat[1].tehtavia + kurssi.osat[2].tehtavia} tehtävää</p>
 
 const Osa = ({ osa }) => <p>{osa.nimi} {osa.tehtavia}</p>
 
