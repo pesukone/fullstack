e1974ad12983c845dd5ceb28c5526db7dcd768fa
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-01-17 15:05:54 +0200

    1.7

--- osa1/unicafe/src/components/Stats.js
+++ osa1/unicafe/src/components/Stats.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const keskiarvo = ({ hyva, neutraali, huono }) => {
+  if (hyva + neutraali + huono === 0) {
+    return 0
+  }
+  
+  return (hyva - huono) / (hyva + neutraali + huono)
+}
+
+const positiivisia = ({ hyva, neutraali, huono }) => {
+  if (hyva + neutraali + huono === 0) {
+    return 0
+  }
+
+  return 100 * (hyva / (hyva + neutraali + huono))
+}
+
 const Stats = ({ tila }) => {
-  console.log(tila)
+  const { hyva, neutraali, huono } = tila
   return (
     <div>
       <h1>statistiikka</h1>
-      <p>hyvä: {tila.hyva}</p>
-      <p>neutraali: {tila.neutraali}</p>
-      <p>huono: {tila.huono}</p>
+      <p>hyvä: {hyva}</p>
+      <p>neutraali: {neutraali}</p>
+      <p>huono: {huono}</p>
+      <p>keskiarvo: {keskiarvo(tila)}</p>
+      <p>positiivisia: {positiivisia(tila)} %</p>
     </div>
   )
 }
