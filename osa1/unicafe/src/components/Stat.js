4e12e6c75f055ad2df0a8f641e048bd1f76d1f9b
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-01-17 15:28:35 +0200

    1.8

---
+++ osa1/unicafe/src/components/Stat.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Stat = ({ nimi, arvo }) => <p>{nimi}: {arvo}</p>
+
+export default Stat
--- osa1/unicafe/src/components/Stats.js
+++ osa1/unicafe/src/components/Stats.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+
+import Stat from './Stat'
 
 const keskiarvo = ({ hyva, neutraali, huono }) => {
   if (hyva + neutraali + huono === 0) {
@@ -19,14 +20,22 @@ const positiivisia = ({ hyva, neutraali, huono }) => {
 
 const Stats = ({ tila }) => {
   const { hyva, neutraali, huono } = tila
-  return (
-    <div>
+    /*return (
       <h1>statistiikka</h1>
       <p>hyvä: {hyva}</p>
       <p>neutraali: {neutraali}</p>
       <p>huono: {huono}</p>
       <p>keskiarvo: {keskiarvo(tila)}</p>
       <p>positiivisia: {positiivisia(tila)} %</p>
+  )*/
+  return (
+    <div>
+      <h1>statistiikka</h1>
+      <Stat nimi="hyvä" arvo={hyva} />
+      <Stat nimi="neutraali" arvo={neutraali} />
+      <Stat nimi="huono" arvo={huono} />
+      <Stat nimi="keskiarvo" arvo={keskiarvo(tila)} />
+      <Stat nimi="positiivisia" arvo={positiivisia(tila) + " %"} />
     </div>
   )
 }
