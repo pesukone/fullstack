220b54cb03477ce88279ebe0b8daa4abb011d7b8
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-02-22 12:34:50 +0200

    5.6

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -8,10 +8,50 @@ const BlogList = ({ blogs }) => (
   </div>
 )
 
-const Blog = ({blog}) => (
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false,
+      blog: props.blog
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    //const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    return (
+      <div style={blogStyle}>
+        <div onClick={this.toggleVisibility}>
+          {this.state.blog.title} {this.state.blog.author}
+        </div>
+        <div style={showWhenVisible}>
+          <p><a href={this.state.blog.url}>{this.state.blog.url}</a></p>
+          <p>{this.state.blog.likes} likes <button>like</button></p>
+          <p>added by {this.state.blog.user.name}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+  /*const Blog = ({blog}) => (
   <div>
     {blog.title} {blog.author}
   </div>  
-)
+)*/
 
 export default BlogList
